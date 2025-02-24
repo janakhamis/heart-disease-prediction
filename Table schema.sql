8cc120254c52540306c101ffa22b88425ed24c93
Bhavesh-Heetoo <BhaveshH1234@gmail.com>
2025-02-24 18:25:47 -0500

    Adding SQL schema and starter code for webapp

---
+++ Table schema.sql
@@ -0,0 +1,18 @@
+-- This is the table schema for storing heart disease data in PostgreSQL
+
+CREATE TABLE heart_disease_data (
+    age INT,
+    gender INT,
+    cp INT,
+    trestbps INT,
+    chol INT,
+    fbs INT,
+    restecg INT,
+    thalachh INT,
+    exang INT,
+    oldpeak FLOAT,
+    slope INT,
+    ca INT,
+    thal INT,
+    risk INT
+);
